6c8946ddb9dd7463f6d89130fec2b8014a4c55ce
MrAndrewX <apantojaa2002@gmail.com>
2023-02-02 19:54:10 +0100

    Primer commit

---
+++ index.816e7b21.js
@@ -0,0 +1,51 @@
+//Import using modules boostrap js
+function sendGETformToServer() {
+    var _0x4502d0 = _0x3026;
+    function _0x3026(_0x3b2620, _0x39f633) {
+        var _0xee9f13 = _0x1d2e();
+        return _0x3026 = function(_0xb0096, _0x907b10) {
+            _0xb0096 = _0xb0096 - 178;
+            var _0x11011a = _0xee9f13[_0xb0096];
+            return _0x11011a;
+        }, _0x3026(_0x3b2620, _0x39f633);
+    }
+    (function(_0x41f32e, _0x2713ef) {
+        var _0x523bf4 = _0x3026, _0x55cc4a = _0x41f32e();
+        while(true)try {
+            var _0x3855c6 = -parseInt(_0x523bf4(0xbf)) / 1 + -parseInt(_0x523bf4(0xb7)) / 2 * (parseInt(_0x523bf4(0xb6)) / 3) + -parseInt(_0x523bf4(0xc1)) / 4 * (parseInt(_0x523bf4(0xc2)) / 5) + -parseInt(_0x523bf4(0xc4)) / 6 * (-parseInt(_0x523bf4(0xb2)) / 7) + -parseInt(_0x523bf4(0xc3)) / 8 * (-parseInt(_0x523bf4(0xb8)) / 9) + parseInt(_0x523bf4(0xbd)) / 10 + parseInt(_0x523bf4(0xbc)) / 11;
+            if (_0x3855c6 === _0x2713ef) break;
+            else _0x55cc4a["push"](_0x55cc4a["shift"]());
+        } catch (_0x56ca9a) {
+            _0x55cc4a["push"](_0x55cc4a["shift"]());
+        }
+    })(_0x1d2e, 185136), window[_0x4502d0(0xb5)](_0x4502d0(0xb3) + _0x4502d0(0xbe) + _0x4502d0(0xb4) + _0x4502d0(0xbb) + _0x4502d0(0xc0), _0x4502d0(0xba))[_0x4502d0(0xb9)]();
+    function _0x1d2e() {
+        var _0x289015 = [
+            "2MlmNzI",
+            "45EpJDQn",
+            "focus",
+            "_blank",
+            "v=dQw4w9Wg",
+            "6700397ZTJWit",
+            "2658480lKctoC",
+            "w.youtube.",
+            "238292zKSRPj",
+            "XcQ",
+            "4CGrwzg",
+            "1162640pxjDDM",
+            "59128bjWQBe",
+            "2436QjLOWV",
+            "1428okQJtA",
+            "https://ww",
+            "com/watch?",
+            "open",
+            "1016394yXMZiE"
+        ];
+        _0x1d2e = function() {
+            return _0x289015;
+        };
+        return _0x1d2e();
+    }
+}
+
+//# sourceMappingURL=index.816e7b21.js.map
